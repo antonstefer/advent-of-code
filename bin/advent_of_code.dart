2e15e92859c4a19fcaae3e784e03adf0f375bb6d
Anton Stefer <stefer@lotum.de>
2021-12-09 00:10:11 +0100

    Read number input

--- bin/advent_of_code.dart
+++ bin/advent_of_code.dart
@@ -1,3 +1,20 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  final result = readNumbersFromFile('input/1.txt');
+  print(result);
+}
+
+List<int> readNumbersFromFile(String path) {
+  try {
+    final result = File(path)
+        .readAsStringSync()
+        .split('\n')
+        .map((s) => int.parse(s))
+        .toList();
+    return result;
+  } on FormatException catch (e) {
+    print('Could not read file: $e');
+    return [];
+  }
 }
